03e9aa28266ffc0a496f43901f4b41be182331b9
chan3pi <167065034+chan3pi@users.noreply.github.com>
2024-04-18 08:39:52 +0300

    Update settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -36,9 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'social_django',
-    'django_extensions',
+    'blog'
 ]
 
 MIDDLEWARE = [
