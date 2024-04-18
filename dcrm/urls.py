113bdf8a1fefb12164b2af2bc2384ec90f102158
chan3pi <167065034+chan3pi@users.noreply.github.com>
2024-04-18 08:39:24 +0300

    Update urls.py

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -21,5 +21,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("blog.urls")),
-    path('social_auth/',include('social_django.urls',namespace="social"))
 ]
